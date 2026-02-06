fc9dea054d53c7bcd2346e5af203dc501c0acbbc
waynemldz <waynegabriel13@gmail.com>
2026-02-06 12:03:05 -0300

    objects

---
+++ metodos.js
@@ -0,0 +1,7 @@
+let marca = "nike";
+
+console.log(marca.toUpperCase())
+
+let marca2 = marca.toLowerCase();
+
+console.log(marca2)
---
+++ objetos.js
@@ -0,0 +1,41 @@
+let pessoa = {
+    nome: "Wayne",
+    profissao: "Programador",
+    idade: 24,
+    mergulhar: function(){
+        console.log("pluft pluft")
+    }
+}
+
+
+let carro = {
+    marca: "VW",
+    portas: 4,
+    eletrico: false,
+    motor: 1.0
+}
+// adicionando e deletando propriedades
+delete carro.portas
+carro.tetoSolar = true;
+
+// copiar propriedades
+
+let objetoA = {
+    prop1: "teste",
+    prop2: "teste2"
+}
+
+let objetoB = {
+    prop3: "teste3"
+}
+
+Object.assign(objetoA, objetoB);
+
+// armazena as chaves em um array
+let array = Object.keys(objetoA);
+array.push("teste")
+console.log(array)
+
+// copiar objeto
+
+let objetoC = objetoA;
