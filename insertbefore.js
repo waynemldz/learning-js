43a194bc79a5b72e0a0a89cbbda481c1ab920cf0
waynemldz <waynegabriel13@gmail.com>
2026-03-02 21:28:24 -0300

    DOM

---
+++ insertbefore.js
@@ -0,0 +1,5 @@
+let novoElemento = document.createElement('p');
+let elementoAlvo = document.querySelector()
+let elementoPai = document.querySelector()
+
+elementoPai.insertBefore(novoElemento, elementoAlvo)
